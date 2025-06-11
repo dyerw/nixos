93979309d05f7213ce169baabd596b71e9242aaa
Liam Dyer <dev@liamdyer.com>
2025-06-11 10:19:23 -0500

    Cleanup leftover template comments

--- system/default.nix
+++ system/default.nix
@@ -57,17 +57,6 @@
     sessionVariables.NIXOS_OZONE_WL = 1;
   };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
   virtualisation.docker.enable = true;
 
   system.stateVersion = "24.11";
